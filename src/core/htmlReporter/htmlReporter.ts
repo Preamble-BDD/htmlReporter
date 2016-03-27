67d85cb1cf672537b17362a084d83763011ee8b4
Jeff Schwartz <jefftschwartz@gmail.com>
2016-03-27 16:59:48 -0400

    Now conditionally creating the summary container.

--- src/core/htmlReporter/htmlReporter.ts
+++ src/core/htmlReporter/htmlReporter.ts
@@ -3,6 +3,26 @@ import Q = require("q");
 let testContainer = "preamble-test-container";
 let configOptions;
 
+let createElement = (tagName: string) => {
+    return document.createElement(tagName);
+};
+
+let createTextNode = (text: string) => {
+    return document.createTextNode(text);
+};
+
+let getElementById = (id: string) => {
+    return document.getElementById(id);
+};
+
+let getTestContainer = (): HTMLElement => {
+    return document.getElementById(testContainer);
+};
+
+let getUiTestContainerEl = (): HTMLElement => {
+    return document.getElementById(configOptions.uiTestContainerId);
+};
+
 class HtmlReporter implements IReporter {
     onErrorFnPrev;
     summaryInfo;
@@ -26,28 +46,22 @@ class HtmlReporter implements IReporter {
         return false;
 
     }
-    createElement(tagName: string) {
-        return document.createElement(tagName);
-    }
-    createTextNode(text: string) {
-        return document.createTextNode(text);
-    }
-    getTestContainer(): HTMLElement {
-        return document.getElementById(testContainer);
-    }
-    getUiTestContainerEl(): HTMLElement {
-        return document.getElementById(configOptions.uiTestContainerId);
-    }
     reportBegin(confOpts: { uiTestContainerId: string, name: string }) {
         configOptions = confOpts;
     }
     reportSummary(summaryInfo: { totDescribes: number, totExcDescribes: number, totIts: number, totFailedIts: number, totExcIts: number, name: string }) {
-        let summary = `<div id="summary">
-        <span>${summaryInfo.name}: </span>
-        <span style="color: blue;">${summaryInfo.totIts}</span><b> specs</b>,
-        <span style="color: blue;">${summaryInfo.totFailedIts}</span><b> failures</b>,
-        <span style="color: blue;">${summaryInfo.totExcIts}</span><b> excluded</b>`;
-        this.getTestContainer().innerHTML = summary;
+        let summaryEl = getElementById("preamble-summary");
+        if (!summaryEl) {
+            summaryEl = createElement("div");
+            summaryEl.setAttribute("id", "preamble-summary");
+            summaryEl.style.height = "1.5em";
+            summaryEl.style.lineHeight = "1.5em";
+            summaryEl.style.marginBottom = "auto";
+            summaryEl.style.backgroundColor = "blue";
+            summaryEl.style.color = "white";
+            getTestContainer().insertAdjacentElement("afterbegin", summaryEl);
+        }
+        summaryEl.innerHTML = `<span>${summaryInfo.name}: </span> <span style="color: white;">${summaryInfo.totIts}</span><b> specs</b>, <span style="color: white;">${summaryInfo.totFailedIts}</span><b> failures</b>, <span style="color: white;">${summaryInfo.totExcIts}</span><b> excluded</b>`;
     }
 }
 
